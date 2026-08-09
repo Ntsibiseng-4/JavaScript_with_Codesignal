8e5d7a48198f508c026b97a4da1815993dbb12e3
Ntsibiseng <naleshabana@gmail.com>
2026-08-09 20:32:55 +0200

    Declaring variables commit

---
+++ first.js
@@ -0,0 +1,6 @@
+// TODO: Declare a variable to keep track of how many cookies we've made
+let cookiesTrack;
+// A constant value representing the oven temperature
+const ovenTemperature = 180;
+// TODO: Write a command to display the oven temperature
+console.log("The oven temperature while baking is: ", ovenTemperature);
