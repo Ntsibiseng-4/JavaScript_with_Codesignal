1af836cf9a46ba710c2feea4431e04fe1f01f5c2
Ntsibiseng <naleshabana@gmail.com>
2026-08-09 20:33:44 +0200

    Created bakings.js
    Write all the instructions for baking chocolate chip cookies, but be sure to set your oven to the right temperature and let everyone know what you're doing.

---
+++ bakings.js
@@ -0,0 +1,8 @@
+// Welcome to the code kitchen! Let's bake some code cookies.
+// TODO: Declare a variable for the type of cookies you're baking. What about chocolate chips?
+let cookies;
+cookies = "chocolate chips";
+// TODO: Set a constant for the oven temperature. Hint: Cookies bake well at 356 degrees Fahrenheit.
+const ovenTemp = 365;
+// TODO: Use console.log to announce the type of cookies and the baking temperature.
+console.log(cookies, "are being baked at", ovenTemp, "degrees");
