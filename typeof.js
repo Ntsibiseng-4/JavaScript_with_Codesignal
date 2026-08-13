6367aeeb7267bf9a430d9612e5609a0eed7e7b6a
Ntsibiseng <naleshabana@gmail.com>
2026-08-13 07:28:19 +0200

    Create typeof.js first commit

---
+++ typeof.js
@@ -0,0 +1,10 @@
+const colorOfMars = "red"; // Mars is red
+let distanceFromSun = 227.9; // 227.9, in million kilometers
+let marsHasLife = false; // Mars doesn't have life (yet)
+
+// TODO: Use 'typeof' to check the type of the variable representing the planet color
+console.log(typeof colorOfMars);
+// TODO: Use 'typeof' to check the type of the variable representing the distance to the Sun
+console.log(typeof distanceFromSun);
+// TODO: Use 'typeof' to check the type of the variable representing whether Mars has life
+console.log(typeof marsHasLife);
