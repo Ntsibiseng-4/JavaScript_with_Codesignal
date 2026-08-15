50bd80c9aa63a54dad86a1f41f1d027feba05358
Ntsibiseng <naleshabana@gmail.com>
2026-08-15 17:50:14 +0200

    Created operators.js

---
+++ operators.js
@@ -0,0 +1,6 @@
+let earthDiameter = 12742; // represents Earth diameter, in kms
+let marsDiameter = 6779; // represents Mars diameter, in kms
+let venusDiameter = 12104; // represents Venus diameter, in kms
+
+// TODO: Compare if Earth is larger than Mars AND larger than Venus, then log the result.
+console.log(earthDiameter > marsDiameter && earthDiameter > venusDiameter);
